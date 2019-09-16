e5f6e08b2df2e4d81efb26e8268ccbe0fbb495a7
Florian Lefevre <florianlefevre@live.fr>
2019-09-16 22:43:14 +0200

    Update WebbleMarkupSimplifier.java
    clean cleanup WebbleMarkupSimplifier and add Javadocs

--- src/main/java/fr/flef/goyave/webble/WebbleMarkupSimplifier.java
+++ src/main/java/fr/flef/goyave/webble/WebbleMarkupSimplifier.java
@@ -5,9 +5,7 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -20,54 +18,84 @@ import org.jdom2.Namespace;
 import org.jdom2.filter.ElementFilter;
 import org.jdom2.input.DOMBuilder;
 import org.jdom2.output.XMLOutputter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
-/** Simplify WordProcessingML xml markup. */
+/** Simplify WordProcessingML XML markup file. */
 public class WebbleMarkupSimplifier
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebbleMarkupSimplifier.class);
+    
     /** w: namespace. */
-    private final static Namespace NS_W = Namespace.getNamespace("w", "http://schemas.openxmlformats.org/wordprocessingml/2006/main");
+    private final static Namespace NS_W = Namespace.getNamespace("w",
+            "http://schemas.openxmlformats.org/wordprocessingml/2006/main");
     /** xml: namespace. */
     private static final Namespace NS_XML = Namespace.getNamespace("xml", "http://www.w3.org/XML/1998/namespace");
 
-    
+    /** Util class. */
     private WebbleMarkupSimplifier()
     {
-        
+
     }
-    
-    public static String simplifyContent(String xmlContent)
+
+    /**
+     * Simplifies WordProcessingML XML markup file by removing Proof, RsidInfo, Bookmarks and adjacentRuns.
+     */
+    public static void simplifyContent(Document doc)
+    {
+        removeProof(doc);
+        removeRsidInfo(doc);
+        removeBookmarks(doc);
+        mergeAdjacentRuns(doc);
+    }
+
+    /**
+     * Creates a {@link Document} object from an XML content represented as a {@link String}.
+     * 
+     * @throws IOException if content cannot be parsed to a valid {@link Document}.
+     */
+    public static Document toDocument(String xmlContent) throws IOException
     {
         try
         {
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
-            
+
             org.w3c.dom.Document w3cDoc = builder.parse(new InputSource(new StringReader(xmlContent)));
-            
-            Document doc = new DOMBuilder().build(w3cDoc);
-            
-            removeProof(doc);
-            removeRsidInfo(doc);
-            removeBookmarks(doc);
-            mergeAdjacentRuns(doc);
-            
-            
+
+            return new DOMBuilder().build(w3cDoc);
+        }
+        catch (SAXException | IOException | ParserConfigurationException e)
+        {
+            throw new IOException("Cannot parse given XML content to a valid DOM Document.", e);
+        }
+
+    }
+
+    /**
+     * Serializes a {@link Document} object to its {@link String} representation.
+     * 
+     * @throws IllegalStateException if document cannot be serialize to a {@link String}.
+     */
+    public static String toString(Document doc)
+    {
+        try
+        {
             XMLOutputter xmlOutput = new XMLOutputter();
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             xmlOutput.output(doc, baos);
             return baos.toString(StandardCharsets.UTF_8.toString());
         }
-        catch (SAXException | IOException | ParserConfigurationException e)
+        catch (IOException e)
         {
-            return xmlContent;
+            LOGGER.error("Cannot write xml content to memory. Is memory full ?", e);
+            throw new IllegalStateException(e);
         }
-        
     }
 
-
-    // Removes w:proofErr from document (SpellChecks)
+    /** Removes w:proofErr from document (SpellChecks). */
     private static void removeProof(Document doc)
     {
         List<Element> toRemove = new ArrayList<>();
@@ -75,12 +103,12 @@ public class WebbleMarkupSimplifier
         toRemove.forEach(Element::detach);
     }
 
-    
-    // Removes w:rsid* from document (used by word to merge document between users)
+    /** Removes w:rsid* from document (used by word to merge document between users). */
     private static void removeRsidInfo(Document doc)
     {
         List<Attribute> toRemove = new ArrayList<>();
-        doc.getDescendants().forEach(c -> {
+        doc.getDescendants().forEach(c ->
+        {
             if (c instanceof Element)
             {
                 Element e = (Element) c;
@@ -89,72 +117,78 @@ public class WebbleMarkupSimplifier
         });
         toRemove.forEach(Attribute::detach);
     }
-    
+
+    /** Removes bookmark* from document. */
     private static void removeBookmarks(Document doc)
     {
         List<Element> nodesToRemove = new ArrayList<>();
         doc.getDescendants(new ElementFilter("bookmarkStart")).forEach(nodesToRemove::add);
         doc.getDescendants(new ElementFilter("bookmarkEnd")).forEach(nodesToRemove::add);
-        
+
         nodesToRemove.forEach(Element::detach);
     }
-    
+
+    /** Merges two consecutive runs if properties are equals. */
     private static void mergeAdjacentRuns(Document contentAsXml)
     {
         List<Element> ps = new ArrayList<>();
         contentAsXml.getRootElement().getDescendants(new ElementFilter("p")).forEach(ps::add);
-        
+
         List<Element> toRemove = new ArrayList<>();
-        Map<Element, List<Element>> toAdd = new HashMap<>();
-        
+
         for (Element p : ps)
         {
             List<Element> pChildrend = p.getChildren();
-            for (int i = 0, j = 1; i < pChildrend.size() - 2 && j < pChildrend.size(); i++, j++)
+            for (int i = 0, j = 1; i < pChildrend.size() - 1 && j < pChildrend.size(); i++, j++)
             {
                 Element n = pChildrend.get(i);
                 Element m = pChildrend.get(j);
-                if (n.getName().equals("r") && m.getName().equals("r") 
-                        && n.getChildren().stream()
-                        .anyMatch(e -> e.getName().equals("t"))
-                        && m.getChildren().stream()
-                        .anyMatch(e -> e.getName().equals("t")) )
-                {
-                   if (areEquals(n.getChild("rPr", NS_W), m.getChild("rPr", NS_W)))
-                   {
-                       
-                       String nContent = n.getChildText("t", NS_W);
-                       String mContent = m.getChildText("t", NS_W);
-                       n.getChild("t", NS_W).setText(nContent + mContent);
-                       if (nContent.startsWith(" ") || mContent.endsWith(" "))
-                       {
-                           n.setAttribute("space", "preserve", NS_XML);
-                       }
-                       toRemove.add(m);
-                       i--;
-                       
-                       System.err.println("Merge : '" + nContent + "', '" + mContent + "'");
-                   }
-                   else
-                   {
-                       System.err.println("Keep : " + pChildrend.get(i).getName());
-                   }
-                }
-                else
-                {
-                    System.err.println("Keep : " + pChildrend.get(i).getName());
-                }
+
                 
+                // IF NODES ARE TEXTUALS (CONTAIN TEXT)
+                if (n.getName().equals("r") && m.getName().equals("r")
+                        && n.getChildren("t", NS_W).size() > 0
+                        && m.getChildren("t", NS_W).size() > 0)
+                {
+                    // PREVIOUS NODE HAS BEEN REMOVED.
+                    // SKIP IT and ROLLBACK SECOND POINTER FOR NEXT LOOP.
+                    if (n.getAttribute("REMOVED") != null)
+                    {
+                        j--;
+                        continue;
+                    }
+                    
+                    // PROPERTIES ARE EQUALS, LET'S MERGE CONTENT
+                    if (areEquals(n.getChild("rPr", NS_W), m.getChild("rPr", NS_W)))
+                    {
+
+                        String nContent = n.getChildText("t", NS_W);
+                        String mContent = m.getChildText("t", NS_W);
+                        n.getChild("t", NS_W).setText(nContent + mContent);
+                        if (nContent.startsWith(" ") || mContent.endsWith(" "))
+                        {
+                            n.setAttribute("space", "preserve", NS_XML);
+                        }
+                        toRemove.add(m);
+                        m.setAttribute("REMOVED", "REMOVED");
+                        i--;
+                    }
+                    // ELSE KEEP BOTH NODES
+                }
+             // ELSE KEEP BOTH NODES
             }
         }
-        
+
         toRemove.forEach(Element::detach);
     }
-    
+
+    /** Returns true if given element are strictly equal (element, content, attributes). 
+     * @throws IllegalStateException if equality cannot be tested (Node are serialized to String).
+     */
     private static boolean areEquals(Element n, Element m)
     {
-        if (n == null && m != null 
-         || n != null && m == null)
+        if (n == null && m != null
+                || n != null && m == null)
         {
             return false;
         }
@@ -162,24 +196,24 @@ public class WebbleMarkupSimplifier
         {
             return true;
         }
-        
+
         XMLOutputter xmlOutput = new XMLOutputter();
         ByteArrayOutputStream nXml = new ByteArrayOutputStream();
         ByteArrayOutputStream mXml = new ByteArrayOutputStream();
         try
         {
             xmlOutput.output(n, nXml);
-            xmlOutput.output(m, mXml); 
-            
+            xmlOutput.output(m, mXml);
+
             String nXmlString = nXml.toString();
             String mXmString = mXml.toString();
-            
+
             return nXmlString.equals(mXmString);
         }
         catch (IOException e)
         {
-            return false;
-        } 
+            LOGGER.error("Cannot write xml content to memory. Is memory full ?", e);
+            throw new IllegalStateException(e);
+        }
     }
-
 }
